5b653e69c4e4f4f6ad75b966c267c955094c7bc8
Victor Selischev <95778687+VictorSelischev@users.noreply.github.com>
2022-12-18 06:38:45 +0200

    Update contactsSlice.jsx

--- src/redux/contactsSlice.jsx
+++ src/redux/contactsSlice.jsx
@@ -1,14 +1,16 @@
 import { createSlice, nanoid } from '@reduxjs/toolkit';
 
 const contactsInitialState = {
-  contacts: [
-    //   { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-    //   { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-    //   { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-    //   { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-  ],
-  isLoader: false,
-  error: null,
+  contacts: {
+    items: [
+      //   { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      //   { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      //   { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      //   { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    isLoader: false,
+    error: null,
+  },
 };
 
 export const contactsSlice = createSlice({
@@ -30,7 +32,9 @@ export const contactsSlice = createSlice({
       },
     },
     deleteContact(state, action) {
-      const index = state.contacts.findIndex(contact => contact.id === action.payload);
+      const index = state.contacts.findIndex(
+        contact => contact.id === action.payload
+      );
       state.contacts.splice(index, 1);
     },
   },
