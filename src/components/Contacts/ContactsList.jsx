52c9118107f2e246c2d66e121f81309b95799a8b
Виктор Селищев <95778687+VictorSelischev@users.noreply.github.com>
2022-12-20 15:38:07 +0200

    create logic in hard selectors

--- src/components/Contacts/ContactsList.jsx
+++ src/components/Contacts/ContactsList.jsx
@@ -1,20 +1,21 @@
 import css from './ContactsList.module.css';
 import { ContactElement } from './ContactElement/ContactElement';
 import { useSelector } from 'react-redux';
-import { selectContacts, selectFilter } from 'redux/selectors';
+// import { selectContacts, selectFilter } from 'redux/selectors';
+import { selectVisibleContacts } from 'redux/selectors';
 
-const getVisibleContacts = (contacts, filter) => {
-  const normalizedFilter = filter.toLowerCase();
+// const getVisibleContacts = (contacts, filter) => {
+//   const normalizedFilter = filter.toLowerCase();
 
-  return contacts.filter(contact =>
-    contact.name.toLowerCase().includes(normalizedFilter)
-  );
-};
+//   return contacts.filter(contact =>
+//     contact.name.toLowerCase().includes(normalizedFilter)
+//   );
+// };
 
 export const ContactsList = () => {
-  const contacts = useSelector(selectContacts);
-  const filter = useSelector(selectFilter);
-  const visibleContacts = getVisibleContacts(contacts, filter);
+  // const contacts = useSelector(selectContacts);
+  // const filter = useSelector(selectFilter);
+  const visibleContacts = useSelector(selectVisibleContacts);
 
   return (
     <ul className={css.contact__list}>
--- src/redux/selectors.jsx
+++ src/redux/selectors.jsx
@@ -2,3 +2,11 @@ export const selectContacts = state => state.contacts.contacts.items;
 export const selectIsLoading = state => state.contacts.contacts.isLoading;
 export const selectError = state => state.contacts.contacts.error;
 export const selectFilter = state => state.filter;
+
+export const selectVisibleContacts = state => {
+  const normalizedFilter = selectFilter(state).toLowerCase();
+  const contacts = selectContacts(state)
+
+    return contacts.filter(contact =>
+        contact.name.toLowerCase().includes(normalizedFilter));
+};
